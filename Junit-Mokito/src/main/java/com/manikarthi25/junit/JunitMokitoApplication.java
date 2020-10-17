b7ad0ac0d922918a226244a548c05ae59bc8a71b
mani <manikarthi25@gmail.com>
2020-10-17 15:01:46 +0530

    commit

---
+++ Junit-Mokito/src/main/java/com/manikarthi25/junit/JunitMokitoApplication.java
@@ -0,0 +1,13 @@
+package com.manikarthi25.junit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitMokitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitMokitoApplication.class, args);
+	}
+
+}
---
+++ Junit-Mokito/src/test/java/com/manikarthi25/junit/JunitMokitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manikarthi25.junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitMokitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
